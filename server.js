10ecae041caccb8816423bd9b9b779de55ab60b7
hdheini <1984586+HdHeini@users.noreply.github.com>
2025-09-07 17:02:11 +0200

    express server static running

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 console.log("Starting server ...");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000, () => {
+  console.log("Server started.");
+});
